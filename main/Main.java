889a3432e9d9d6133fd05ce3de55cb91681da9b9
selfstudymaster <webselfmaster@gmail.com>
2021-06-21 23:58:56 +0900

    設問モード

--- main/Main.java
+++ main/Main.java
@@ -1,4 +1,31 @@
 package main;
+// H29春季 基本情報 午後 Java
+// [プログラム 1]
+/* (a) */ class TierTable {
+    final double[][] pairs;
+
+    TierTable(double... tiers) {
+        if (tiers.length % 2 == 1) {
+            throw new IllegalAccessException("不正な長さ" + tiers.length);
+        }
+    }
+
+}
+
+
+// [プログラム ]
+
+// [プログラム ]
+
+
+// [プログラム ]
+
+
+// [プログラム ]
+
+
+// [プログラム ]
+
 
 public class Main {
 
